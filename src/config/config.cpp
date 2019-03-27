2c6f74a800748f0223a134ec4b6ce1489351c1ec
Rémon S <6475387+Coffee2CodeNL@users.noreply.github.com>
2019-03-27 10:38:16 +0100

    Release v0.0.5 (#18)
    * Update README.md to reflect version bump
    
    * Update to README.md, new shields
    
    Added AUR Package shield
    Changed gitter.im shield
    
    * Added support for oblique gesture swipes.
    
    To add the support the config class was modified to include the new
    gestures types: left_up, right_up, left_down, right_down.
    Also changed the way the commands are saved in the config class, so
    there won't be a need for a lot of if statements to handle 4 finger
    swipes and 3 finger swipes.
    Finally, the handle_swipe_event_without_coords was modified to detect
    the oblique swipes.
    
    * Commenting the code a bit and some minor cosmetic and semantic changes
    
    - Added comments to each function
    - Increased string array size to 10 in config.h even though we only need 9 slots, I'm being a bit pedantic about this
    - Adding some build flags to CMakeLists.txt to show every error, preferably we'd get clean output eventually
    - Renamed find_gesture_device to gesture_device_exists in Input class
    - Renamed find_config_file to config_file_exists as this actually describes the logic of this function in Config class
    - Renamed find_home_folder to find_config_file in Config class in preparation to adding /etc/gebaar/gebaard.toml to list of possible config file locations
    
    * Prepare for release by bumping up version in README.md
    
    * Let warnings be warnings, release v0.0.4
    
    * Goodbye Gitter, Hello Discord.
    
    * Fix config location (#13)
    
    If XDG_CONFIG_HOME is set, .config will be appended a second time
    resulting in /home/example/.config/.config/gebaar/gebaard.toml
    
    This is fixed by only appending .config, when the HOME directory
    workaround is used.
    
    Fixes #12
    
    * Fix nullptr conversion to std::string (#16)
    
    Instead of using the result of getenv directly, we must check first, if
    getenv returned a nullptr, otherwise the program will crash.
    
    Regression from 41c7c05
    
    * Add the build directory to gitignore (#15)
    
    The README build instructions use this directory.
    
    * Add a desktop file (#14)
    
    Users can copy this desktop file to $XDG_CONFIG_HOME/autostart to enable
    autostart on most desktop environments.

--- src/config/config.cpp
+++ src/config/config.cpp
@@ -19,6 +19,7 @@
 
 #include <zconf.h>
 #include "config.h"
+#include "../util.h"
 
 /**
  * Check if config file exists at current path
@@ -68,17 +69,21 @@ void gebaar::config::Config::load_config()
  */
 bool gebaar::config::Config::find_config_file()
 {
-    const char* temp_path;
-    temp_path = getenv("XDG_CONFIG_HOME");
-    if (temp_path==nullptr) {
-        temp_path = getenv("HOME");
-        if (temp_path==nullptr) {
+    std::string temp_path = gebaar::util::stringFromCharArray(getenv("XDG_CONFIG_HOME"));
+    if (temp_path.empty()) {
+        // first get the path to HOME
+        temp_path = gebaar::util::stringFromCharArray(getenv("HOME"));
+        if (temp_path.empty()) {
             temp_path = getpwuid(getuid())->pw_dir;
         }
+        // then append .config
+        if (!temp_path.empty()) {
+            temp_path.append("/.config");
+        }
     }
-    if (temp_path!=nullptr) {
+    if (!temp_path.empty()) {
         config_file_path = temp_path;
-        config_file_path.append("/.config/gebaar/gebaard.toml");
+        config_file_path.append("/gebaar/gebaard.toml");
         return true;
     }
     return false;
